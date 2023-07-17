c681c96ab7d4fb17cf24cfe3f842686c0ed3d262
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-17 13:11:55 +0200

    made form structure && made it work
    if u type something it shows in an alert

--- src/js/Dictionary.js
+++ src/js/Dictionary.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Dictionary.css";
 
 export default function Dictionary() {
-  return <div>hello</div>;
+  let [keyword, setKeyword] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`searching ${keyword}`);
+  }
+  function changeValue(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Text" onChange={changeValue} />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 }
