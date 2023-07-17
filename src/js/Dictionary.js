a72afb1e5644a36d3f9ca2c4d73c708846749c07
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-17 14:04:10 +0200

    integrated api
    changed names of the functions  && integrated api in the component

--- src/js/Dictionary.js
+++ src/js/Dictionary.js
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "../css/Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`searching ${keyword}`);
+    search();
   }
-  function Search(event) {
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  function search() {
+    let apiKey = "ecb1e604034014f9c94ecdtbo8ae777f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
   return (
     <div className="Dictionary">
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Text" onChange={Search} />
-        <input type="submit" value="Login" />
+        <input
+          type="text"
+          placeholder="Enter a keyword"
+          onChange={handleKeywordChange}
+        />
+        <input type="submit" value="Search" />
       </form>
     </div>
   );
