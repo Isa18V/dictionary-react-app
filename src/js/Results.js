7b9b314b6eae246d7f7209a41ec4ce973de773ff
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-18 15:05:24 +0200

    changed loop again

--- src/js/Results.js
+++ src/js/Results.js
@@ -4,24 +4,22 @@ import "../css/Results.css";
 
 export default function Results(props) {
   if (props.results) {
-    const uniquePartOfSpeech = [];
+    const uniquePartOfSpeech = [0];
 
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.meanings
-          .map((meaning) => {
-            if (!uniquePartOfSpeech.includes(meaning.partOfSpeech)) {
-              uniquePartOfSpeech.push(meaning.partOfSpeech);
-              return (
-                <div key={meaning.partOfSpeech}>
-                  <Meaning meaning={meaning} />
-                </div>
-              );
-            }
-            return null;
-          })
-          .filter(Boolean)}
+        {props.results.meanings.map((meaning) => {
+          if (!uniquePartOfSpeech.includes(meaning.partOfSpeech)) {
+            uniquePartOfSpeech.push(meaning.partOfSpeech);
+            return (
+              <div key={meaning.partOfSpeech}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          }
+          return null;
+        })}
       </div>
     );
   } else {
