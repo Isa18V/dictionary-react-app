224d295c6a4eef5bab9e532490ad1627354e2d5e
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-07-18 14:00:13 +0200

    added api calls in meanings component

--- src/js/Meaning.js
+++ src/js/Meaning.js
@@ -4,10 +4,29 @@ export default function Meaning(props) {
   return (
     <div className="meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <p>
-        <em>{props.meaning.example}</em>
-      </p>
+      <div>
+        <p>
+          <strong>Definition: </strong> {props.meaning.definition}
+        </p>
+        <p>
+          <strong>Phonetic: </strong> {props.meaning.phonetic}
+        </p>
+        <p>
+          <strong>Example: </strong> <em>{props.meaning.example}</em>
+        </p>
+        <ul>
+          {props.meaning.synonyms && (
+            <li>
+              <strong>Synonyms: </strong> {props.meaning.synonyms.join(", ")}
+            </li>
+          )}
+          {props.meaning.antonyms && (
+            <li>
+              <strong> Antonyms: </strong> {props.meaning.antonyms.join(", ")}
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
